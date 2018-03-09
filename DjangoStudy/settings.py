3d4c14d668fca7fb2ae48569fbb3e8e84fd51ff3
donghao1995 <donghao19950925@gmail.com>
2018-03-09 16:17:39 +0800

    up

--- DjangoStudy/settings.py
+++ DjangoStudy/settings.py
@@ -37,12 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
     'django_filters',
     'blog',
-
-    # 'taggit',
 ]
 
 MIDDLEWARE = [
